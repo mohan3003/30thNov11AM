adb981b2ef29c5f60a58a5e0f3d645297a6974e1
kannanbatch <mohansacet07@gmail.com>
2021-12-05 20:13:04 +0530

    I Changed myself

--- src/test/java/org/runnerclass/RunnerClass1.java
+++ src/test/java/org/runnerclass/RunnerClass1.java
@@ -10,5 +10,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources",snippets=SnippetType.CAMELCASE,glue="org\\stepdefinition",dryRun=true)
 public class RunnerClass1{
-	
+	private void newOne() {
+		System.out.println("Conflict should come");
+		System.out.println("Un ista mayiruku lam poda mudiyathu");
+	}
 }
